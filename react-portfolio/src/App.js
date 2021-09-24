9829a9c8c6c3dfa47f538ceb41fcae692ef10440
Nathan da Silva <pacsmacck@gmail.com>
2021-09-23 21:20:48 -0700

    sections added

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam quaerat adipisci id at minus. Pariatur exercitationem adipisci perspiciatis qui atque eaque quas recusandae reiciendis, quam ut assumenda dolorem mollitia iste.</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Quam, consectetur tempore ipsa ex officiis culpa eaque, sequi perferendis nam itaque quis aut blanditiis laboriosam exercitationem. Minima consectetur excepturi quaerat natus, accusantium neque iusto labore? Et enim officia quae!</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>A, minima delectus? Necessitatibus tempora sequi laborum eos placeat deleniti, laudantium atque labore saepe maxime, hic, repudiandae perspiciatis minus autem beatae. Et, quo deleniti fugit a accusamus distinctio sequi quasi?</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Quis recusandae dolores aspernatur tenetur libero sapiente, enim vel tempora molestiae molestias blanditiis eligendi alias dignissimos harum. Dolor in eius, possimus itaque ut, magnam voluptates veritatis excepturi dolore rem velit!</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Vero sunt eaque dicta eius aspernatur cupiditate fugiat autem dolorem? Ullam obcaecati quae, rerum perferendis, est quisquam quam porro temporibus dolores enim unde pariatur omnis facilis commodi asperiores libero ea.</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Voluptas quaerat nobis culpa voluptate, rerum unde atque! Adipisci quae maiores, incidunt labore assumenda necessitatibus ab eos possimus, culpa vero perspiciatis. Quasi rerum delectus dolorem explicabo doloribus perferendis odio voluptatum.</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Sint, porro doloremque quod illo at dignissimos et? At velit ab corporis vel officiis, sunt facere accusamus itaque dolores ipsam sequi aliquam fuga asperiores. Nemo sunt natus harum illo dolores.</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Temporibus ad quod esse minima earum voluptate eos delectus ipsa, obcaecati perspiciatis magni, iste quia, sunt atque velit fugit aut culpa. Quo maiores dignissimos qui, corporis suscipit iure minus ea.</p>
+      </section>
+
+      <footer></footer>
+
+
     </div>
   );
 }
