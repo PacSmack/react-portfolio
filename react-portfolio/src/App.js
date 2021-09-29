e15b4e6fe0337be51bb8ca115e20d3f6263130ab
Nathan da Silva <pacsmacck@gmail.com>
2021-09-29 16:53:26 -0700

    interests component added to the page

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
-import Header from './components/HeaderComponent'
-import About from './components/AboutComponent'
-import Projects from './components/ProjectsComponent'
-import Technologies from './components/TechnologiesComponent'
+import Header from './components/HeaderComponent';
+import About from './components/AboutComponent';
+import Projects from './components/ProjectsComponent';
+import Technologies from './components/TechnologiesComponent';
+import Interests from './components/InterestsComponent';
+import Contact from './components/ContactComponent';
 
 
 function App() {
@@ -14,12 +16,9 @@ function App() {
       <About />
       <Projects />
       <Technologies />
+      <Interests />
 
       
-      <section className="emerald">
-        <h1>Nice Curves</h1>
-        <p>Vero sunt eaque dicta eius aspernatur cupiditate fugiat autem dolorem? Ullam obcaecati quae, rerum perferendis, est quisquam quam porro temporibus dolores enim unde pariatur omnis facilis commodi asperiores libero ea.</p>
-      </section>
       <section className="honeyYellow">
         <h1>Nice Curves</h1>
         <p>Voluptas quaerat nobis culpa voluptate, rerum unde atque! Adipisci quae maiores, incidunt labore assumenda necessitatibus ab eos possimus, culpa vero perspiciatis. Quasi rerum delectus dolorem explicabo doloribus perferendis odio voluptatum.</p>
---
+++ react-portfolio/src/components/InterestsComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Interests() {
+
+    return (
+        <section className="emerald">
+            <h1>Interests</h1>
+        </section>
+    )
+}
