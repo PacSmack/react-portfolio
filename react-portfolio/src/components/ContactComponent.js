abd4834e4ff608e418fe80320e998ca9a083e62a
Nathan da Silva <pacsmacck@gmail.com>
2021-10-04 23:38:08 -0700

    v2 portfolio

--- react-portfolio/src/components/ContactComponent.js
+++ react-portfolio/src/components/ContactComponent.js
@@ -6,41 +6,50 @@ import resumeIcon from '../assets/resume.png';
 import resumePdf from '../assets/Resume.pdf';
 
 export default function Contact() {
-    
+
 
     return (
         <section className="contact" id="contact">
             <h1>Let's Connect!</h1>
 
             <div className="contactWrapper">
-                <div className="contactItem">
-                    <a href="mailto: nathandsbr@gmail.com" target="_blank">
-                        <div>
-                            <h2>Email me!</h2>
-                            <img src={emailIcon} alt="" />
+                <a href="mailto: nathandsbr@gmail.com" target="_blank">
+                    <div className="contactItem">
+                        <div className="centered-div">
+                            <div>
+                                <h2>Email me!</h2>
+                                <img src={emailIcon} alt="" />
+                            </div>
                         </div>
-                    </a>
-                </div>
+                    </div>
+                </a>
 
-                <div className="contactItem">
-                    <a href={resumePdf} target="_blank">
-                        <div>
-                            <h2>Check out my resume!</h2>
-                            <img src={resumeIcon} alt="" />
+                <a href={resumePdf} target="_blank">
+                    <div className="contactItem">
+                        <div className="centered-div">
+                            <div>
+                                <h2>Check out my resume!</h2>
+                                <img src={resumeIcon} alt="" />
+                            </div>
                         </div>
-                    </a>
-                </div>
+                    </div>
+                </a>
 
-                <div className="contactItem">
-                    <a href="https://www.linkedin.com/in/nathan-dias-silva/" target="_blank">
-                        <div>
-                            <h2>Get in touch on Linkedin!</h2>
-                            <img src={linkedinIcon} alt="" />
+                <a href="https://www.linkedin.com/in/nathan-dias-silva/" target="_blank">
+                    <div className="contactItem">
+                        <div className="centered-div">
+                            <div>
+                                <h2>Get in touch on Linkedin!</h2>
+                                <img src={linkedinIcon} alt="" />
+                            </div>
                         </div>
-                    </a>
-                </div>
+                    </div>
+                </a>
             </div>
-
+<div className="last-phrase">
+            <h1>“Judge of a man by his questions rather than by his answers.”</h1>
+            <h2>-Voltaire</h2>
+</div>
 
 
             <div class="custom-shape-divider-top-1632964461">
@@ -57,6 +66,6 @@ export default function Contact() {
                     <path d="M0,0V3.6H580.08c11,0,19.92,5.09,19.92,13.2,0-8.14,8.88-13.2,19.92-13.2H1200V0Z" class="shape-fill"></path>
                 </svg>
             </div>
-        </section>
+        </section >
     )
 }
